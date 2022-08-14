893fca291962bd94b284cc68cf38dbc6862cb29b
Alex <asigalov61@hotmail.com>
2022-08-14 01:40:05 -0700

    Update GPT2RGAX.py

--- GPT2RGAX.py
+++ GPT2RGAX.py
@@ -135,10 +135,10 @@ rpr = False #'store_true'
 
 enable_rpr = True
 max_seq = 1024
-n_layers = 6
-num_heads = 8
-d_model = 512
-dim_feedforward = 512
+n_layers = 16
+num_heads = 16
+d_model = 1024
+dim_feedforward = 1024
 dropout_prob = 0.1
 
 ########################################################
